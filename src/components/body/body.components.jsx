521f8775480933d0459615be391d1c5773808985
Hugo Pereira <hmdjmp@gmail.com>
2025-03-18 20:27:00 +0000

    site structure

---
+++ src/components/body/body.components.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Body(){
+    return (
+        <div className="body-container">body
+            <div>
+                <i className="list"></i>
+                <i className="bi-alarm"></i>
+            </div>
+        </div>
+    );
+}
+
+export default Body;
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './assets/styles/index.css';
+import './assets/styles/header.css';
+import './assets/styles/footer.css';
+import './assets/styles/body.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
