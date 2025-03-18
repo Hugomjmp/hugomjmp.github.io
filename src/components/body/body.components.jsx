48c7a36b21ba35f2b87af7cb0d6f13f3e5ed168a
Hugo Pereira <hmdjmp@gmail.com>
2025-03-18 20:44:00 +0000

    site structure

--- src/components/body/body.components.jsx
+++ src/components/body/body.components.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 function Body(){
     return (
-        <div className="body-container">body
-            <div>
-                <i className="list"></i>
-                <i className="bi-alarm"></i>
+        <div className="body-container">
+            <div className="body">
+                body
             </div>
         </div>
     );
